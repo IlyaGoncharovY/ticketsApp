af71d95f6c3beb36342fd49947486e0c2b6b91f3
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-04-27 00:30:25 +0300

    fix(filter) refactor style

--- src/features/tickets/TicketsComponent.tsx
+++ src/features/tickets/TicketsComponent.tsx
@@ -2,15 +2,17 @@ import {useAppSelector} from '../../store';
 
 import {TicketItem} from './item/TicketItem.tsx';
 
+import s from './TicketsComponent.module.css';
+
 export const TicketsComponent = () => {
 
   const tickets = useAppSelector(state => state.ticketsReducer.tickets);
 
   return (
-    <div>
+    <div className={s.ticketsContainer}>
       {tickets.map((ticket, index) =>
         <TicketItem
-          key={index} 
+          key={index}
           ticket={ticket}
         />,
       )}
