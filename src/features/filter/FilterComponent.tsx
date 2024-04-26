70fca2792b64c7ee4a0b4291c5df257d7bd973a0
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-04-27 00:30:05 +0300

    fix(filter) refactor style, added basic structure

--- src/features/filter/FilterComponent.tsx
+++ src/features/filter/FilterComponent.tsx
@@ -1,7 +1,40 @@
+import s from './FilerComponent.module.css';
+
 export const FilterComponent = () => {
   return (
-    <div>
-        FilterComponent
+    <div className={s.filterContainer}>
+      <div className={s.buttonContainer}>
+        <div>
+            Валюта
+        </div>
+        <div>
+          <button>RUB</button>
+          <button>USD</button>
+          <button>EUR</button>
+        </div>
+      </div>
+      <div className={s.checkBoxContainer}>
+        <div>
+            Колличество пересадок
+        </div>
+        <div className={s.checkBoxItemContainer}>
+          <span>
+            <input type={'checkbox'}/> Все
+          </span>
+          <span>
+            <input type={'checkbox'}/> Без пересадок
+          </span>
+          <span>
+            <input type={'checkbox'}/> 1 пересадка
+          </span>
+          <span>
+            <input type={'checkbox'}/> 2 пересадки
+          </span>
+          <span>
+            <input type={'checkbox'}/> 3 пересадки
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
