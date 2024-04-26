803e0e1c133a1275d2c41f865180b3896a313809
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-04-26 23:39:26 +0300

    feat(app) render HeaderComponent, FilterComponent, TicketsComponent

--- src/features/app/App.tsx
+++ src/features/app/App.tsx
@@ -1,8 +1,14 @@
+import {HeaderComponent} from '../header';
+import {FilterComponent} from '../filter';
+import {TicketsComponent} from '../tickets';
+
 export function App() {
 
   return (
-    <>
-
-    </>
+    <div>
+      <HeaderComponent/>
+      <FilterComponent/>
+      <TicketsComponent/>
+    </div>
   );
 }
