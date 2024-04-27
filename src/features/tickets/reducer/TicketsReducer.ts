66d9735fc119109a17c8ae5fab24fa1e030f6c35
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-04-27 11:25:52 +0300

    fix(ticket reducer) added ticketSelection function

--- src/features/tickets/reducer/TicketsReducer.ts
+++ src/features/tickets/reducer/TicketsReducer.ts
@@ -1,22 +1,31 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 import {ticketsData, TicketsType} from '../../../common';
 
 interface initialStateType {
-    tickets: TicketsType[]
+    tickets: TicketsType[];
+    selectedFilters: number[];
 }
 
 const initialState: initialStateType = {
   tickets: ticketsData,
+  selectedFilters: [],
 };
 
 const TicketsSlice = createSlice({
   name: 'ticketsSlice',
   initialState,
   reducers: {
-
+    ticketSelection: (state, action: PayloadAction<number[]>) => {
+      state.selectedFilters = action.payload;
+      if (state.selectedFilters.includes(-1)) {
+        state.tickets = ticketsData;
+      } else {
+        state.tickets = ticketsData.filter(ticket => state.selectedFilters.includes(ticket.stops));
+      }
+    },
   },
 });
-export const {} = TicketsSlice.actions;
+export const {ticketSelection} = TicketsSlice.actions;
 
 export default TicketsSlice.reducer;
