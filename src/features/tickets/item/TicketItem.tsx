0c5a9a8d2fb226ebdce5aa6f6e9decaca78dae08
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-04-30 23:00:21 +0300

    fix(tickets item) use component for render parts item

--- src/features/tickets/item/TicketItem.tsx
+++ src/features/tickets/item/TicketItem.tsx
@@ -1,86 +1,34 @@
-import {FC} from 'react';
+import {FC, memo} from 'react';
 
 import {TicketsType} from '../../../common';
+import {TicketLeft} from '../components/left/TicketLeft.tsx';
 
-import TKImg from '../../../../src/assets/TKlogo.png';
-import BAImg from '../../../../src/assets/BAlogo.png';
-import S7Img from '../../../../src/assets/S7logo.jpg';
-import SUImg from '../../../../src/assets/SUlogo.png';
-import airLine from '../../../assets/lineAirImg.jpg';
+import {TicketRight} from '../components/right/TicketRight.tsx';
 
 import s from './TicketItem.module.css';
-
-export const LOGO_DATA = {
-  'BA': BAImg,
-  'S7': S7Img,
-  'SU': SUImg,
-  'TK': TKImg,
-};
 
 interface ITicketItem {
     ticket: TicketsType
 }
 
-export const TicketItem: FC<ITicketItem> = ({ticket}) => {
-
-  const onClickHandler = (price: number) => {
-    alert(`цена за перелёт: ${price} рублей`);
-  };
+export const TicketItem: FC<ITicketItem> = memo(({ticket}) => {
 
   return (
     <div className={s.ticketItemContainer}>
-      <div className={s.priceContainer}>
-        <div>
-          <img
-            src={LOGO_DATA[ticket.carrier]}
-            alt={'logo airlines'}
-            className={s.logo}
-          />
-        </div>
-        <div className={s.buttonContainer}>
-          <button
-            className={s.buttonItem}
-            onClick={() => onClickHandler(ticket.price)}
-          >
-            <div>
-              <div>Купить</div>
-              <div>за {ticket.price}</div>
-            </div>
-          </button>
-        </div>
-      </div>
-      <div>
-        <div className={s.descriptionUp}>
-          {ticket.departure_time}
-        </div>
-        <div className={s.descriptionDown}>
-          {ticket.origin}, {ticket.origin_name}
-        </div>
-        <div style={{opacity: '0.7'}}>
-          {ticket.departure_date}, ПТ
-        </div>
-      </div>
-      <div className={s.logoContainer}>
-        <div>
-          {ticket.stops} пересадок
-        </div>
-        <img
-          src={airLine} alt="line air"
-          width={'190px'}
-          style={{opacity: '0.7'}}
-        />
-      </div>
-      <div>
-        <div className={s.descriptionUp}>
-          {ticket.arrival_time}
-        </div>
-        <div className={s.descriptionDown}>
-          {ticket.destination_name}, {ticket.destination}
-        </div>
-        <div style={{opacity: '0.7'}}>
-          {ticket.arrival_time}, ПТ
-        </div>
-      </div>
+      <TicketLeft
+        carrier={ticket.carrier}
+        price={ticket.price}
+      />
+      <TicketRight
+        departure_time={ticket.departure_time}
+        origin={ticket.origin}
+        origin_name={ticket.origin_name}
+        departure_date={ticket.departure_date}
+        stops={ticket.stops}
+        arrival_time={ticket.arrival_time}
+        destination_name={ticket.destination_name}
+        destination={ticket.destination}
+      />
     </div>
   );
-};
+});
